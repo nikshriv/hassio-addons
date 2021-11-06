c53632726d86429e455aa8f972834f2c4a479f97
nikshriv <nikshriv@gmail.com>
2021-11-06 15:19:09 -0700

    Add files via upload

---
+++ cync_lights_addon/rootfs/etc/cont-init.d/uuidgen.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Lutron Certificate
+# Ensure we have a valid UUID to work with
+# ==============================================================================
+dbus-uuidgen --ensure
