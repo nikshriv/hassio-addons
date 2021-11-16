3adf546062c4a9234c3d5407c626307ad8dcfb0d
nikshriv <nikshriv@gmail.com>
2021-11-16 06:54:15 -1000

    Add files via upload

---
+++ cync_lights_addon/index.js
@@ -0,0 +1,27 @@
+const WebSocket = require('ws').WebSocket
+
+connectToHomeAssistant()
+
+function connectToHomeAssistant(){
+	haWebsocket = new WebSocket('ws://supervisor/core/websocket')
+
+	haWebsocket.on('message',function(data){
+		resp = JSON.parse(data.toString())
+		switch (resp.type){
+			case 'auth_required':
+				// log in to home assistant
+				haWebsocket.send('{"type":"auth","access_token":"' + token + '"}')
+				break
+			case 'auth_ok':
+				haWebsocket.send('{"id":"2","type":"subscribe_events","event_type":"state_changed"}') // subscribe to cync switch state changes
+				break
+			case 'event': // receive home assistant switch state changes
+				if (resp.id == 2) {
+                    console.log(resp.event.data)
+                }
+		}
+	})
+	haWebsocket.on('error',function(err){
+		console.log(err)
+	})
+}
