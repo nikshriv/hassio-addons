5413c633e4d1c2b2ad6eaaccd4cf224e5340fd30
nikshriv <nikshriv@gmail.com>
2021-11-16 06:49:39 -1000

    Delete cync_lights_addon directory

--- cync_lights_addon/rootfs/etc/cont-init.d/uuidgen.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Custom Add-on: Cync Server
-# Ensure we have a valid UUID to work with
-# ==============================================================================
-# dbus-uuidgen --ensure
