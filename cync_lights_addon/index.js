15a201bfd76ac73c20924eafa9ef6857ddb02a7b
nikshriv <nikshriv@gmail.com>
2021-12-16 19:18:45 -0800

    Add files via upload

--- cync_lights_addon/index.js
+++ cync_lights_addon/index.js
@@ -10,6 +10,9 @@ var app = express()
 
 var googleAssistant = null
 var cbygeTcpServer = null
+var cync_room_data = null
+var cync_credentials = null
+var google_credentials = null
 var config = null
 var entry_id = null
 
@@ -30,22 +33,22 @@ function monitorCbygeSwitches(cync_credentials) {
 					var power = data.readUInt8(index + 16) > 0
 					var brightness = data.readUInt8(index + 17)
 					var deviceId = data.readUInt32BE(index + 5).toString()
-					if (config.cync_room_data.switchID_to_room[deviceId]){
-						var room = config.cync_room_data.switchID_to_room[deviceId]
-						if (power != config.cync_room_data.rooms[room].switches[deviceId].state || brightness != config.cync_room_data.rooms[room].switches[deviceId].brightness){
-							config.cync_room_data.rooms[room].switches[deviceId].state = power
-							config.cync_room_data.rooms[room].switches[deviceId].brightness = brightness
+					if (cync_room_data.switchID_to_room[deviceId]){
+						var room = cync_room_data.switchID_to_room[deviceId]
+						if (power != cync_room_data.rooms[room].switches[deviceId].state || brightness != cync_room_data.rooms[room].switches[deviceId].brightness){
+							cync_room_data.rooms[room].switches[deviceId].state = power
+							cync_room_data.rooms[room].switches[deviceId].brightness = brightness
 							var currentStateAll = power
-							for (let sw in config.cync_room_data.rooms[room].switches){
-								if (config.cync_room_data.rooms[room].switches[sw].state != currentStateAll || config.cync_room_data.rooms[room].switches[sw].brightness != brightness){currentStateAll = !power}
+							for (let sw in cync_room_data.rooms[room].switches){
+								if (cync_room_data.rooms[room].switches[sw].state != currentStateAll || cync_room_data.rooms[room].switches[sw].brightness != brightness){currentStateAll = !power}
 							}
 							if (currentStateAll == power){
-								config.cync_room_data.rooms[room].state = power
-								config.cync_room_data.rooms[room].brightness = brightness
+								cync_room_data.rooms[room].state = power
+								cync_room_data.rooms[room].brightness = brightness
 								var state = power ? 'on':'off'
-								var stateInfo = power ? {'entity_id':config.cync_room_data.rooms[room].entity_id,'brightness':Math.round(brightness*255/100)} : {'entity_id':config.cync_room_data.rooms[room].entity_id}
-								if (config.cync_room_data.rooms[room].entity_id != ''){
-									console.log('Updating ' + config.cync_room_data.rooms[room].entity_id + ' to ' + state + ' with brightness ' + brightness.toString())
+								var stateInfo = power ? {'entity_id':cync_room_data.rooms[room].entity_id,'brightness':Math.round(brightness*255/100)} : {'entity_id':cync_room_data.rooms[room].entity_id}
+								if (cync_room_data.rooms[room].entity_id != ''){
+									console.log('Updating ' + cync_room_data.rooms[room].entity_id + ' to ' + state + ' with brightness ' + brightness.toString())
 									http.post('http://supervisor/core/api/services/light/turn_' + state, stateInfo, {headers: {Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN}})
 									.catch(function(err){console.log(err.message)})
 								}						
@@ -97,7 +100,7 @@ function startGoogleAssistant(credentials){
 function googleAssistantQuery(room,state,brightness){
 	if (googleAssistant){
 		var queries = []
-		var switchNames = config.cync_room_data.rooms[room].switch_names
+		var switchNames = cync_room_data.rooms[room].switch_names
 		for (var i = 0; i < switchNames.length; i++){
 			if (brightness){
 				sendQuery("Set " + switchNames[i] + " to " + brightness.toString() + "%",i)
@@ -117,6 +120,12 @@ function sendQuery(query,count){
 	},count*300)
 }
 
+function writeEntryId(){
+	files.writeFile('entry_id.json',JSON.stringify({'entry_id':entry_id}),function(err){
+		console.log(err)
+	})
+}
+
 //At addon startup, check if the Cync Lights Integration was previously installed and configured, then reload the Integration to initialize this addon
 if (files.existsSync('entry_id.json')){
 	entry_id = JSON.parse(files.readFileSync('entry_id.json','utf8')).entry_id
@@ -131,19 +140,13 @@ if (files.existsSync('entry_id.json')){
 			}
 		})
 		if (entry_id){
-			files.writeFileSync('entry_id.json',JSON.stringify({'entry_id':entry_id}))
+			writeEntryId()
 			http.post('http://supervisor/core/api/services/homeassistant/reload_config_entry', {'entry_id':entry_id}, {headers: {Authorization: 'Bearer ' + process.env.SUPERVISOR_TOKEN}})		
 		} else {
 			console.log('Please install and configure the Cync Lights Integration')			
 		}
 	}).catch(function(err){
 		console.log('Unable to connect to home assistant')		
-	})
-}
-
-function writeEntryId(){
-	files.writeFile('entry_id.json',JSON.stringify({'entry_id':entry_id}),function(err){
-		console.log(err)
 	})
 }
 
@@ -159,26 +162,30 @@ app.post('/init', function (req, res) {
 		cbygeTcpServer.destroy()
 		cbygeTcpServer = null
 	}
-	config = null
+	cync_room_data = null
+	cync_credentials = null
+	google_credentials = null
 	res.send('Cync Addon initiated')
 })
 app.post('/setup', function (req, res){
 	var room = req.body.room
 	var room_data = req.body.room_data
-	if (!config){
-		config = req.body.setup_data
+	if (!cync_room_data || !cync_credentials || !google_credentials){
+		cync_room_data = req.body.cync_room_data
+		cync_credentials = req.body.cync_credentials
+		google_credentials = req.body.google_credentials
 	}
-	if (config.cync_room_data.rooms[room]){
-		config.cync_room_data.rooms[room] = room_data
+	if (cync_room_data.rooms[room]){
+		cync_room_data.rooms[room] = room_data
 		console.log("Added " + JSON.stringify(room_data))
 	} else {
 		console.log('Unable to add data for ' + room)
 	}
 	if (!cbygeTcpServer){
-		monitorCbygeSwitches()
+		monitorCbygeSwitches(new Uint8Array(cync_credentials))
 	}
 	if (!googleAssistant){
-		startGoogleAssistant()
+		startGoogleAssistant(google_credentials)
 	}
 	if (!entry_id){
 		entry_id = req.body.entry_id
@@ -188,8 +195,8 @@ app.post('/setup', function (req, res){
 })
 app.post('/turn-on', function (req, res) {
 	var room = req.body.room
-	if (!config.cync_room_data.rooms[room].state){
-		config.cync_room_data.rooms[room].state = true
+	if (!cync_room_data.rooms[room].state){
+		cync_room_data.rooms[room].state = true
 		googleAssistantQuery(room,true)
 	}
 	res.send('Received state update')
@@ -197,21 +204,21 @@ app.post('/turn-on', function (req, res) {
 app.post('/set-brightness', function (req, res) {
 	var room = req.body.room
 	var brightness = req.body.brightness
-	if (!config.cync_room_data.rooms[room].state){
-		config.cync_room_data.rooms[room].state = true
-		config.cync_room_data.rooms[room].brightness = brightness
+	if (!cync_room_data.rooms[room].state){
+		cync_room_data.rooms[room].state = true
+		cync_room_data.rooms[room].brightness = brightness
 		googleAssistantQuery(room,true,brightness)
-	} else if (config.cync_room_data.rooms[room].state && config.cync_room_data.rooms[room].brightness != brightness) {
-		config.cync_room_data.rooms[room].brightness = brightness
+	} else if (cync_room_data.rooms[room].state && cync_room_data.rooms[room].brightness != brightness) {
+		cync_room_data.rooms[room].brightness = brightness
 		googleAssistantQuery(room,true,brightness)
 	}
 	res.send('Received state update')
 })
 app.post('/turn-off', function (req, res) {
 	var room = req.body.room
-	if (config.cync_room_data.rooms[room].state){
-		config.cync_room_data.rooms[room].state = false
-		config.cync_room_data.rooms[room].brightness = 0
+	if (cync_room_data.rooms[room].state){
+		cync_room_data.rooms[room].state = false
+		cync_room_data.rooms[room].brightness = 0
 		googleAssistantQuery(room,false)
 	}
 	res.send('Received state update')
